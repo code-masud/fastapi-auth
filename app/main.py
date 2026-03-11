bfef0279eb09e9d409d419c41156fdf5bca2fcc4
code-masud <code.masud@hotmail.com>
2026-03-11 23:36:26 +0600

    CORS Middleware

--- app/main.py
+++ app/main.py
@@ -1,16 +1,24 @@
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import auth_router, post_router, user_router, like_router
-from .database import engine, Base
-from .config import settings
 
-# Base.metadata.create_all(bind=engine)
+origins = ["*"]
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get('/')
 def root():
     return {'message': 'hello world!'}
+
 
 app.include_router(user_router.router)
 app.include_router(auth_router.router)
